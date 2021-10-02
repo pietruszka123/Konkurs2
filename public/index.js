6bc45f53ad5b5f5721403160d0dd6b7aabbd8223
pietruszka123 <56603721+pietruszka123@users.noreply.github.com>
2021-10-02 14:57:09 +0200

    0.0.1

--- public/index.js
+++ public/index.js
@@ -1,1 +1,24 @@
-import 
+console.log(Quagga)
+Quagga.init({
+    inputStream: {
+        name: "Live",
+        type: "LiveStream",
+        target: document.querySelector('#yourElement'), // Or '#yourElement' (optional)
+        constraints: {
+            width: 100,
+            height: 100,
+            facingMode: "environment",
+            deviceId: "7832475934759384534"
+        },
+    },
+    decoder: {
+        readers: ["code_128_reader"]
+    }
+}, function(err) {
+    if (err) {
+        console.log(err);
+        return
+    }
+    console.log("Initialization finished. Ready to start");
+    Quagga.start();
+});
