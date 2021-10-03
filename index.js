ef4566a15dec68089fdcea9ceda94c1fa58d4acd
Gami13 <71034355+Gami13@users.noreply.github.com>
2021-10-03 14:44:48 +0200

    Odczytywanie i wyswietlanie kodu

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
-const express = require("express");
-var app = express();
-var server = app.listen(8080, function() {
-    console.log("Dziala")
-})
-app.use("/", express.static('public'));
+Quagga.init({
+    inputStream: {
+        name: "Live",
+        type: "LiveStream",
+        target: document.querySelector('#yourElement'), // Or '#yourElement' (optional)
+        constraints: {
+            width: 200,
+            height: 200,
+            facingMode: "environment",
+        },
+    },
+    decoder: {
+        readers: ["upc_reader",
+            "upc_e_reader", "ean_8_reader", "ean_reader", "code_39_reader",
+            "code_39_vin_reader",
+            "codabar_reader",
+
+            "i2of5_reader",
+            "2of5_reader",
+            "code_93_reader", "code_128_reader"
+        ]
+    }
+}, function(err) {
+    if (err) {
+        console.log(err);
+        document.body.innerHTML = err
+        return
+    }
+    console.log("Initialization finished. Ready to start");
+    Quagga.start();
+    /*Quagga.onDetected((e) => {
+        if (e != null) {
+            console.log(e)
+            document.body.innerHTML = "tak"
+        }
+    })*/
+    Quagga.onProcessed((e) => {
+        if (e != null) {
+            const kod = Object.values(e.codeResult.code);
+            console.log(e);
+
+            document.body.innerHTML = kod;
+        }
+
+    })
+
+});
