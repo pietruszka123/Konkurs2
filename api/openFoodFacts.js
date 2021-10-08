c275cc61968fed6a8b5325a7e3731d611a5035e3
pietruszka123 <56603721+pietruszka123@users.noreply.github.com>
2021-10-08 23:15:00 +0200

    test

---
+++ api/openFoodFacts.js
@@ -0,0 +1,30 @@
+const https = require('https')
+var http = require('http');
+const config = require("./config.json");
+
+module.exports.getProduct = async function getProduct(productCode) {
+    return new Promise((resolve, reject) => {
+    var options = {
+        host: 'world.openfoodfacts.org',
+        path: `/api/${config['api-version']}/product/${productCode}.json`
+      };
+      console.log(productCode)
+      var req = http.get(options, function(res) {
+        if(res.statusCode != 200)resolve(JSON.stringify({status:0,status_verbose:"server Error",code:productCode}))
+        var bodyChunks = [];
+        res.on('data', function(chunk) {
+          bodyChunks.push(chunk);
+        }).on('end', function() {
+          var body = Buffer.concat(bodyChunks);
+          resolve(body)
+        })
+      });
+      
+      req.on('error', function(e) {
+        console.log('ERROR: ' + e.message);
+        reject(e)
+      });
+    })
+}
+
+//getProduct('5449000000996');
